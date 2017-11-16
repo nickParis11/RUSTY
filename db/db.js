ea8e619b7ba1bc2ce90a728eede75b59b1edced8
bkleung89 <bkleung89@gmail.com>
2017-11-16 08:30:16 -0800

    heroku db

--- db/db.js
+++ db/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/rustydb', { useMongoClient: true }); // not heroku-friendly
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/rustydb', { useMongoClient: true }); // not heroku-friendly
 // double quotes and single quotes
 
 var db = mongoose.connection;
