47c1864ff0807941f433fc0b5c7cbfee61382b89
bkleung89 <bkleung89@gmail.com>
2017-11-20 11:10:09 -0800

    trying to merge all our changes

---
+++ client/components/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PrimaryHeader from './PrimaryHeader.jsx';
+
+const HomePage = () => {
+  return (
+    <div >
+      <PrimaryHeader />
+      <form>
+        <input type="button" value="Sign In" />
+        <input type="button" value="Sign Up" />
+      </form>
+    </div>
+    )
+}
+export default HomePage;
---
+++ client/components/Landing.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import axios from 'axios';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+
+function Landing(props) {
+}
