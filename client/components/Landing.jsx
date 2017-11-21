40aa3b2610226ec6dfa01b9a7863db9e357ddee6
ben.p <polanskyb@gmail.com>
2017-11-20 16:18:15 -0800

    get rid of ghosts

--- client/components/Landing.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import axios from 'axios';
-import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
-
-function Landing(props) {
-}
--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
   res.setHeader(
     'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
   );
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   next();
