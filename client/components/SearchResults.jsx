1d80820bd4116889af4570b387f8d42c5c6cd0e9
bkleung89 <bkleung89@gmail.com>
2017-11-21 19:49:01 -0800

    refactored search results

--- client/components/SearchResults.jsx
+++ client/components/SearchResults.jsx
@@ -3,7 +3,6 @@ import axios from 'axios';
 import DropDownMenu from 'material-ui/DropDownMenu';
 import MenuItem from 'material-ui/MenuItem';
 import BusinessMini from './BusinessMini.jsx';
-import SearchMenu from './SearchMenu.jsx';
 
 class SearchResults extends React.Component {
   constructor(props) {
@@ -34,37 +33,31 @@ class SearchResults extends React.Component {
   }
 
   render() {
+    var callback = (business) => {
+      return (<BusinessMini
+                business={business}
+                key={business._id}
+                businessName={business.businessName}
+                businessCategory={business.businessCategory}
+                profileImg={business.profileImg}
+      />);
+    };
+    var businesses = this.state.businesses;
     return (
       <div>
-      <DropDownMenu value={this.state.value} onChange={this.handleChange}>
-      <MenuItem value={1} primaryText="All Local Businesses" />
-      <MenuItem value={2} primaryText="Bar/Restaurant" />
-      <MenuItem value={3} primaryText="Cafe" />
-      <MenuItem value={4} primaryText="Hotel" />
-      </DropDownMenu>
-      <div>
-      { this.state.category = 1 ? (
-        this.state.businesses.map((business) => {
-          return (<BusinessMini
-            business={business}
-            key={business._id}
-            businessName={business.businessName}
-            businessCategory={business.businessCategory}
-            profileImg={business.profileImg}
-            />);
-        })
-
-      ) : (
-        this.state.businesses.filter(business => business.category === this.state.value).map((business) => {
-          return (<BusinessMini
-                    business={business}
-                    key={business._id}
-                    businessName={business.businessName}
-                    businessCategory={business.businessCategory}
-                    profileImg={business.profileImg}
-          />);
-        }))
-      };
+        <DropDownMenu value={this.state.value} onChange={this.handleChange}>
+          <MenuItem value={1} primaryText="All Local Businesses" />
+          <MenuItem value={2} primaryText="Bar/Restaurant" />
+          <MenuItem value={3} primaryText="Cafe" />
+          <MenuItem value={4} primaryText="Hotel" />
+        </DropDownMenu>
+        <div>
+          { this.state.category === 1 ? (
+              businesses.map(callback)
+          ) : (
+              businesses.filter(business => business.category === this.state.value)
+                        .map(callback)
+          )};
         </div>
       </div>
     );
