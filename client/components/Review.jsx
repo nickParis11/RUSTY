848a13e74a2d704907170567954bf121f98ca46e
bkleung89 <bkleung89@gmail.com>
2017-11-25 09:41:59 -0800

    removed primary header

--- client/components/Review.jsx
+++ client/components/Review.jsx
@@ -5,7 +5,6 @@ import PrimaryHeader from './PrimaryHeader.jsx';
 const Review = (props) => {
   return (
     <div>
-      <PrimaryHeader />
       <div>
         <p>{props.rating}</p>
         <p>{props.description}</p>
