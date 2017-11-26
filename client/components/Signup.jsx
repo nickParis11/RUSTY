823f6144695701919d6e9837f859c2d6658e1960
Krista <krista.abraham@gmail.com>
2017-11-25 19:26:28 -0500

    Button styling for login and signup

--- client/components/Signup.jsx
+++ client/components/Signup.jsx
@@ -4,18 +4,32 @@ import BusinessSignup from './BusinessSignup.jsx';
 import PetOwnerSignup from './PetOwnerSignup.jsx';
 
 const Signup = (props) => {
+  const linkTextStyle = {
+    fontFamily: 'Roboto, sans-serif',
+    display: 'inline'
+  };
 
 console.log('test retrieving data from app =',props.test)
 console.log('propsfrom app =',props)
 console.log("in sign up before return statement")
   return (
     <BrowserRouter>
-      <div>
-        <h2> IN SIGN UP </h2>
-        <ul>
-          <li><NavLink to="/signup/business" activeClassName="active">Business Owner</NavLink></li>
-          <li><NavLink to="/signup/petOwner" activeClassName="active">Pet Owner</NavLink></li>
-        </ul>
+      <div style={{whiteSpace: 'nowrap', marginTop: 15}}>
+        <h3 style={linkTextStyle}>Sign up as a </h3>
+        <NavLink
+            to="/signup/business"
+            style={linkTextStyle}
+            activeStyle={{ fontWeight: 'bold' }}>
+            Business
+        </NavLink>
+        <h3 style={linkTextStyle}> or a </h3>
+          <NavLink
+            to="/signup/petOwner"
+            style={linkTextStyle}
+            activeStyle={{ fontWeight: 'bold' }}>
+            Pet Owner
+          </NavLink>
+       <h3 style={linkTextStyle}>?</h3>
         <Switch>
           <Route path="/signup/business" render={()=> <BusinessSignup app={props.app}  />} />
           <Route path="/signup/petOwner" render={()=> <PetOwnerSignup app={props.app}  />} />
--- client/components/UserLogin.jsx
+++ client/components/UserLogin.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
-const UserLogin = ({ loginType, authenticateLogin }) => {
+const UserLogin = ({ userType, authenticateLogin }) => {
   const onSubmit = (ev) => {
     ev.preventDefault();
     const email = ev.target.elements.email.value;
     const password = ev.target.elements.password.value;
-    authenticateLogin(email, password, loginType);
+    authenticateLogin(email, password, userType);
     document.getElementById("loginForm").reset();
   };
 
   return (
     <form id="loginForm" onSubmit={onSubmit}>
-      <p>Email: <input name="email" type="email" /></p>
+      <p>{userType} Email: <input name="email" type="email" /></p>
       <p>Password: <input name="password" type="password" /></p>
-      <button type="submit">Sign In</button>
+      <button type="submit">Submit</button>
     </form>
   );
 };
