44ab6d41194ad2b926c3f5a0d809eab0671bc71d
bkleung89 <bkleung89@gmail.com>
2017-11-26 08:31:09 -0800

    added test file

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('Addition', function() {
+  describe('of integers', function() {
+    it('should add two and two', function() {
+      assert.equal(2 + 2, 4);
+    });
+  });
+});
