c639907c7c85a9fe493eedf9ab8aac6b53130c5f
ben.p <polanskyb@gmail.com>
2017-11-26 17:16:42 -0800

    add promofield to business profile and a list of what rusty users are saying

--- client/components/BusinessProfile.jsx
+++ client/components/BusinessProfile.jsx
@@ -1,86 +1,98 @@
 import React from 'react';
+import axios from 'axios';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import {red500, blue500} from 'material-ui/styles/colors';
 import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
 import Paper from 'material-ui/Paper';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
 import ProfileHeader from './ProfileHeader.jsx';
-import SearchResults from './SearchResults.jsx';
-import Review from './Review.jsx';
 
-const paperStyle_2 = {
+const paperStyle = {
   width: 500,
   margin: 10,
   textAlign: 'left',
 };
 
-
-const BusinessProfile = (props) => {
-  console.log('PROPS:', props.reviews)
-  return (
-    <MuiThemeProvider>
-      <div>
-        <ProfileHeader onLogOut={props.onLogOut} />
-        <br />
-        <Paper
-          style={paperStyle_2}
-          zDepth={3}>
-        <Card>
-          <CardHeader
-            title={'Welcome, ' + props.user.businessName}
-          />
-          <img src={props.user.profileImg} style={{maxWidth: 500}} alt="" />
-          <CardMedia
-            overlay={<CardTitle title={props.user.businessName}/>}
-          />
-          <CardText >
-            What people are saying about you:
-            <br />
-            <ul>
-              {props.reviews.map((review) => {
-                return (
-                  <li>{review.description}</li>
-                )
-              })}
-            </ul>
-         </CardText>
-        </Card>
-        </Paper>
-
-
-       </div>
-
-     </MuiThemeProvider>
-  );
+const hintStyle = {
+  textAlign: 'center',
 };
 
+class BusinessProfile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      promoText: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handlePromo = this.handlePromo.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ promoText: event.target.value });
+  }
+
+  handlePromo() {
+    axios.post('/api/promo', {
+      promo: this.state.promoText,
+      businessId: this.props.id,
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        return console.error(err);
+      });
+  }
+
+  render() {
+    return (
+      <MuiThemeProvider>
+        <div>
+          <ProfileHeader onLogOut={this.props.onLogOut} />
+          <br />
+          <Paper
+            style={paperStyle}
+            zDepth={3}
+          >
+            <Card>
+              <CardHeader
+                title={`Welcome, ${this.props.user.businessName}!`}
+              />
+              <img src={this.props.user.profileImg} style={{maxWidth: 500}} alt="" />
+              <CardMedia
+                overlay={<CardTitle title={this.props.user.businessName}/>}
+              />
+              <CardText >
+                What Rusty users are saying about you:
+                <br />
+                <ul>
+                  {this.props.reviews.map((review) => {
+                    return (
+                      <li>{review.description}</li>
+                    );
+                  })}
+                </ul>
+              </CardText>
+              <CardActions>
+                <TextField
+                  hintText="enter a new promotion here!"
+                  hintStyle={{ hintStyle }}
+                  multiLine={true}
+                  value={this.state.promoText}
+                  onChange={event => this.handlePromo(event)}
+                />
+                <FlatButton
+                  label="make your offer"
+                  primary={true}
+                  onClick={() => this.handleSubmit()}
+                />
+              </CardActions>
+            </Card>
+          </Paper>
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
 export default BusinessProfile;
-
-/*
-
-
-   class Profile extends React.Component {
-
-
-
-   render () {
-   //console.log('in PRofile ')
-   let dataz=this.props.parent.state.mockedServerRetrievedData;
-   //console.log('in PRofile dataz = ', dataz)
-
-   return (
-   <div>
-
-   <h2> in Profile component // THIS IS SERVER RETRIEVED DATA </h2>
-
-   Email : {dataz.email || ''} <br/>
-   Name : {dataz.name || ''} <br/>
-   Zip : {dataz.zip || ''} <br/>
-   password : {dataz.password || ''} <br/>
-   Pet : {dataz.pet || ''}
-   </div>
-   )
-   }
-   }
-
-
- */
